15d78b31a24ec78f6cbc492a6151e72a7982052c
Wei-Ming <barry800414@gmail.com>
2018-02-03 16:47:53 +0800

    fisrt commiet

---
+++ index.js
@@ -0,0 +1,51 @@
+const cors = require("cors");
+const expressMongoDb = require("express-mongo-db");
+const express = require('express');
+
+const PORT = parseInt(process.env.PORT) || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/todos';
+const app = express();
+
+async function main() {
+  app.use(cors());
+
+  // setup api routes
+  app.use(expressMongoDb(MONGODB_URI));
+
+  app.get('/todos/', async (req, res) => {
+    const todos = req.db.collection("todos");
+    const todoList = await todos.find().toArray();
+    res.send(todoList);
+  });
+
+  app.post('/todos/', async (req, res) => {
+    const todos = req.db.collection("todos");
+    const result = await todos.insertOne({ title });
+    if (result) {
+      console.log(result);
+      res.send(result);
+    } else {
+      res.send(false);
+    }
+  });
+
+  app.delete('/todos/:id', async (req, res) => {
+    const db = req.db.collection("todos");
+    const _id = req.params.id;
+    const todo = await db.findOne({ _id });
+    if (todo) {
+      const result = await db.deleteOne({ _id });
+      if (result) { res.send(true); }
+      else { res.send(false); }
+    } else {
+      res.send(false);
+    }
+  });
+
+  // start server
+  app.listen(PORT, () => {
+    console.log(`server is running on ${PORT} port...`);
+  });
+}
+
+main();
